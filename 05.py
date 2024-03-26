38c1572e7767881b6408a1c23995262e06a430ce
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2024-03-26 09:24:40 +0700

    ch

--- 05.py
+++ 05.py
@@ -1,6 +1,6 @@
-from functools import reduce
+import functools
 
 a = int(input())
 b = int(input())
 c = int(input())
-print(reduce(lambda x, y: x * y, [x for x in range(a, b + 1) if x ** 0.5 % 1 == 0 and x % c == 0]))
+print(functools.reduce(lambda x, y: x * y, [x for x in range(a, b + 1) if x ** 0.5 % 1 == 0 and x % c == 0]))
